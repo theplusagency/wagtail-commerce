e1eb4edefb4789740ce6d7ad573136181fffa1f2
Rafael Ponieman <rafa@theplus.agency>
2018-03-06 18:43:25 -0300

    added store currency to request

--- wagtailcommerce/frontend/context_processors.py
+++ wagtailcommerce/frontend/context_processors.py
@@ -3,5 +3,6 @@ from wagtailcommerce.stores.utils import get_store
 
 def store(request):
     return {
-        'store': request.store
+        'store': request.store,
+        'store_currency': request.store.currency if request.store else ''
     }
