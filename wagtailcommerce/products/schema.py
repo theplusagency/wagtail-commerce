e7cf8d5d2cec880433498f3c0a978a1b2af924bc
Rafael Ponieman <rafa@theplus.agency>
2018-05-05 20:01:41 -0300

    supergraphene progress

--- wagtailcommerce/products/schema.py
+++ wagtailcommerce/products/schema.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 import graphene
 
 from wagtailcommerce.products.models import Category, Product
@@ -32,8 +33,38 @@ class CategoriesQuery(graphene.ObjectType):
         return categories
 
 
-class ProductsQuery(graphene.ObjectType):
-    products2 = graphene.List(ProductType)
+class BaseProductSearchResult(graphene.ObjectType):
+    num_pages = graphene.Int()
 
-    def resolve_products2(self, info, **kwargs):
-        return Product.objects.specific().all()
+
+class BaseProductsQuery(graphene.ObjectType):
+
+    def get_products_queryset(self, info, *args, **kwargs):
+        products = Product.objects.specific().all().filter(active=True)
+
+        params = kwargs.keys()
+
+        if 'parent_categories' in params and kwargs['parent_categories']:
+            products = products.filter(categories__pk__in=kwargs['parent_categories'])
+
+        return products
+
+    @classmethod
+    def resolve_product_search(cls, info, *args, **kwargs):
+        products = cls.get_products_queryset(info, *args, **kwargs)
+        print('here')
+
+        params = kwargs.keys()
+
+        if 'page_number' in params and params['page_number'].isdigit():
+            if kwargs['page_size'] and kwargs['page_size'].isdigit():
+                page_size = int(kwargs['page_size'])
+        else:
+            page_number = 1
+            page_size = 10
+
+        paginator = Paginator(products, page_size)
+        print(paginator.count)
+        print(paginator.num_pages)
+        print(paginator.object_list)
+        return cls.get_search_result_class(cls)(products=paginator.page(1), num_pages=paginator.num_pages)
