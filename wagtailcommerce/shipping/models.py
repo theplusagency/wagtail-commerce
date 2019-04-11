6047170f29199ccaf2778534976ab0970c2877e7
Rafael Ponieman <rafa@theplus.agency>
2019-04-10 21:06:40 -0300

    removed OCA from code

--- wagtailcommerce/shipping/models.py
+++ wagtailcommerce/shipping/models.py
@@ -1,11 +1,11 @@
+from __future__ import absolute_import, unicode_literals
+
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.functional import cached_property
 from django.utils.translation import ugettext_lazy as _
-
 from modelcluster.fields import ParentalKey
 from modelcluster.models import ClusterableModel
-
 from wagtail.admin.edit_handlers import FieldPanel
 
 from wagtailcommerce.shipping.query import ShipmentQuerySet
@@ -102,6 +102,28 @@ class ShippingMethod(AbstractShippingMethod, ClusterableModel, metaclass=Shippin
         Generate the actual shipment order
         """
         raise NotImplementedError
+
+    @cached_property
+    def specific(self):
+        """
+        Return this product variant in its most specific subclassed form.
+        """
+        # the ContentType.objects manager keeps a cache, so this should potentially
+        # avoid a database lookup over doing self.content_type. I think.
+        content_type = ContentType.objects.get_for_id(self.content_type_id)
+        model_class = content_type.model_class()
+        if model_class is None:
+            # Cannot locate a model class for this content type. This might happen
+            # if the codebase and database are out of sync (e.g. the model exists
+            # on a different git branch and we haven't rolled back migrations before
+            # switching branches); if so, the best we can do is return the page
+            # unchanged.
+            return self
+        elif isinstance(self, model_class):
+            # self is already the an instance of the most specific class
+            return self
+        else:
+            return content_type.get_object_for_this_type(id=self.id)
 
 
 class BaseShipmentManager(models.Manager):
